7a3a1f00976edbde805d7f9e6ca8220b04b08a92
informado <chocolatey030@gmail.com>
2020-05-03 08:57:35 +0200

    files created or changed by acelera

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,9 +1,31 @@
 #!/bin/bash -e
 
+TOOL="${1:-./informado}"
 DELIVERABLE="${DELIVERABLE:-informado}"
 
-main(){
-  echo "helloworld"
+validate(){
+    if [ -z "${TOOL}" ]; then
+        echo "No deliverable defined. Assuming that 'go run main.go' 
+should be run."
+        TOOL="go run main.go"
+    fi
 }
 
+build(){
+  echo "TRAVIS_TAG: '$TRAVIS_TAG' DELIVERABLE: '$DELIVERABLE'"
+  go build -ldflags "-X informado/cmd.Version=${TRAVIS_TAG}" -o "${DELIVERABLE}"
+  $SHA512_CMD "${TOOL}" > "${DELIVERABLE}.sha512.txt"
+  chmod +x "${DELIVERABLE}"
+}
+
+cleanup(){
+  echo "cleanup"
+}
+
+main(){
+  validate
+  build
+}
+
+trap cleanup EXIT
 main
