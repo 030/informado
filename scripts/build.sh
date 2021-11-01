c651d8b39a23eb063739d4e595d13bedcf2c255a
030 <chocolatey030@gmail.com>
2021-11-01 12:31:04 +0100

    [GH-25] Resolve incorrect variable issues in workflow templates.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 TRAVIS_TAG="${TRAVIS_TAG:-local}"
 SHA512_CMD="${SHA512_CMD:-sha512sum}"
-DELIVERABLE="${DELIVERABLE:-informado}"
+export DELIVERABLE="${DELIVERABLE:-informado}"
 
-echo "TRAVIS_TAG: '$TRAVIS_TAG' DELIVERABLE: '$DELIVERABLE'"
+echo "GITHUB_TAG: '$GITHUB_TAG' DELIVERABLE: '$DELIVERABLE'"
 cd cmd/informado
-go build -ldflags "-X informado/cmd.Version=${TRAVIS_TAG}" -o "${DELIVERABLE}"
+go build -ldflags "-X informado/cmd.Version=${GITHUB_TAG}" -o "${DELIVERABLE}"
 $SHA512_CMD "${DELIVERABLE}" >"${DELIVERABLE}.sha512.txt"
 chmod +x "${DELIVERABLE}"
+cd ../..
